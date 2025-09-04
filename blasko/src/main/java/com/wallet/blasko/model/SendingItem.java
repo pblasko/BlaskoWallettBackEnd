4616c07f41fa8702d65c4224b8f38895eb17214f
peterblasko <blasko.peter.hu@gmail.com>
2025-09-04 17:24:52 +0200

    Add all models

---
+++ blasko/src/main/java/com/wallet/blasko/model/SendingItem.java
@@ -0,0 +1,17 @@
+package com.wallet.blasko.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SendingItem {
+
+    private List<Account> accounts;
+    private List<Category> categories;
+    private List<Item> items;
+
+}
